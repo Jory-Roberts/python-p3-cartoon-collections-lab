2a1608c0faec5b57ed07310979a332999fa657bd
Jory Roberts <robertsjory13@gmail.com>
2023-07-11 22:07:32 -0500

    completes roll call dwarves

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,5 +1,9 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarfs):
+    dwarf_list = [f"{index}. {dwarf}" for index, dwarf in enumerate(dwarfs, 1)]
+
+    for dwarf in dwarf_list :
+        print(dwarf)
+
 
 def summon_captain_planet():
     pass
